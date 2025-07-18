004d0ba46899969f32d320ffcb8a851ffd15b630
Vadim Gusev <uppjke@gmail.com>
2025-07-18 12:18:26 +0300

    refactor: simplify layout structure by removing unnecessary flex direction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         </motion.p>
         
         <motion.div 
-          className="flex flex-col sm:flex-row gap-4 justify-center"
+          className="flex gap-4 justify-center"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.7, delay: 0.6 }}
