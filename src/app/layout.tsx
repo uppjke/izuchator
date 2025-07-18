750f87dd935ca0796d3526b95da7c3e4cddcdfe3
Vadim Gusev <uppjke@gmail.com>
2025-07-18 11:02:13 +0300

    feat: update layout metadata and add new logo and favicon assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -12,9 +12,29 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
 export const metadata: Metadata = {
-  title: "Izuchator",
-  description: "Landing page for Izuchator",
+  title: "Изучатор",
+  description: "Учи, Учись, Твори с Изучатором!",
+  icons: {
+    icon: "/logo.svg",
+    shortcut: "/logo.svg",
+    apple: "/logo.svg",
+    other: [
+      {
+        rel: "apple-touch-icon",
+        url: "/logo.svg",
+      },
+      {
+        rel: "apple-touch-icon-precomposed",
+        url: "/logo.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
