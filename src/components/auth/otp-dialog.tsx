992e255a60b76d06333583ce71ef5a7d9bf80edb
Vadim Gusev <uppjke@gmail.com>
2025-07-24 17:59:04 +0300

    refactor: remove unused Button import from OTP dialog component

--- src/components/auth/otp-dialog.tsx
+++ src/components/auth/otp-dialog.tsx
@@ -4,9 +4,8 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { useRef, useState } from 'react'
-import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 
 const schema = z.object({
